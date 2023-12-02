ba4d57cee6c3655dfa41709152f027536c0f17b5
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-02 20:13:53 +0100

    1

--- app/upload-button/page.tsx
+++ app/upload-button/page.tsx
@@ -1,10 +1,40 @@
 "use client";
  
 import { UploadButton } from "../utils/uploadthing";
+
+import { useState } from 'react';
+import Link from 'next/link';
  
 export default function UploadButtonPage() {
+    const [images, setImages] =useState<{
+        fileUrl: string
+        fileKey: string
+    }[]>([])
+
+    const title =images.length ? (
+        <>
+        <p>Upload Complete!</p>
+        <p className='mt-2'>{images.length}files</p>
+        </>
+    ) : null
+
+    const imgList =(
+        <>
+        {title}
+        <ul>
+            {images.map(image=>(
+                <li key={image.fileUrl} className='mt-2'>
+                    <Link href={image.fileUrl} target='_blank'>
+                        {image.fileUrl}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+        </>
+    )
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
       <UploadButton
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
@@ -17,6 +47,7 @@ export default function UploadButtonPage() {
           alert(`ERROR! ${error.message}`);
         }}
       />
+      {imgList}
     </main>
   );
 }
